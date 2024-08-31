2c661c02a4680168ac7d962b790a244de78e65e0
auliadj <auliadwijmrn@gmail.com>
2024-08-31 22:30:31 +0700

    Praktikum 4A - Membuat Class

---
+++ ketiga.php
@@ -0,0 +1,41 @@
+<?php
+class Lingkaran
+{
+    const PHI = 3.14;
+
+    public function luas($jari_jari) : float {
+        return self::PHI*$jari_jari*$jari_jari;
+    }
+    public function keliling($jari_jari) : float {
+        return 2*self::PHI*$jari_jari;
+    }
+}
+
+class Bola
+{
+    const PHI = 3.14;
+    public function volume($jari_jari) : float {
+        return (4/3)*self::PHI*pow($jari_jari,3);
+    }
+}
+
+class Tabung
+{
+    const PHI = 3.14;
+    public function volume($jari_jari,$tinggi) : float {
+        return self::PHI*pow($jari_jari,2)*$tinggi;
+    }
+}
+
+class Kerucut
+{
+    const PHI = 3.14;
+    public function volume($jari_jari,$tinggi) : float {
+        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    }
+}
+$nasi_tumpeng = new Kerucut();
+$jari_jari = 4;
+$tinggi = 10;
+$volume_nasi_tumpeng = $nasi_tumpeng->volume($jari_jari, $tinggi);
+echo "Volume nasi tumpeng: {$volume_nasi_tumpeng} cm^3";
