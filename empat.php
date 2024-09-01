cc2130a25406facce650b19680c6a54b25c1075a
auliadj <auliadwijmrn@gmail.com>
2024-09-01 20:01:44 +0700

    Praktikum 2a - Implementasi Input dan Output

--- empat.php
+++ empat.php
@@ -6,7 +6,10 @@ class Author
 
     public function show($type): array
     {
-        return [];
+        return [
+            'name' => $this->name,
+            'description' => $this->description
+        ];
     }
 }
 
@@ -23,12 +26,25 @@ class Book
 
     public function showAll(): array
     {
-        return [];
+        return [
+            'ISBN' => $this->ISBN,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+            'language' => $this->language,
+            'numberOfPages' => $this->numberOfPages,
+            'author' => $this->author,
+            'publisher' => $this->publisher
+        ];
     }
 
     public function detail($ISBN): array
     {
-        return [];
+        if ($this->ISBN == $ISBN) {
+            return $this->showAll();
+        } else {
+            return [];
+        }
     }
 }
 
@@ -48,3 +64,26 @@ class Publisher
         return $this->phone;
     }
 }
+
+$author = new Author();
+$author->name = 'Tereliye';
+$author->description = 'Penulis yang sudah memiliki banyak buku';
+
+$book = new Book();
+$book->ISBN = 1234567890;
+$book->title = 'Bumi';
+$book->description = 'Persahabatan dan petualangan';
+$book->category = 'Fiksi';
+$book->language = 'Indonesia';
+$book->numberOfPages = 400;
+$book->author = $author->name;
+$book->publisher = 'Gramedia';
+
+$publisher = new Publisher();
+$publisher->name = 'Gramedia';
+$publisher->address = '123 Main St';
+$publisher->setPhone('123-456-7890');
+
+echo "Author: " . $author->show('all') . "\n";
+echo "Book: " . print_r($book->showAll(), true) . "\n";
+echo "Publisher: " . $publisher->name . " - " . $publisher->address . " - " . $publisher->getPhone() . "\n";
